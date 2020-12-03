f3d530198161a88e8570f18a05444b2e26a4fef0
Corey Griffin <corey.griffin@datadoghq.com>
2020-12-02 22:39:08 -0500

    [FEATURE] Adding computer extension attributes endpoint

---
+++ classic/computer_extenstion_attr.go
@@ -0,0 +1,154 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2020 Datadog, Inc.
+
+package classic
+
+import (
+	"bytes"
+	"context"
+	"encoding/xml"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+// ComputerExtensionAttrExists is a helper function to check if an extension attribute
+// exists without having to parse the response. Note: If an error occurs that doesn't include
+// a not found message ... we log the error and return false
+func (j *Client) ComputerExtensionAttrExists(identifier interface{}) bool {
+	_, err := j.ComputerExtensionAttributeDetails(identifier)
+	if err != nil {
+		if !strings.Contains(err.Error(), "The server has not found anything matching the request URI") {
+			j.logger.Errorf("did not find computer extension attribute %v due to %s", identifier, err.Error())
+		}
+		return false
+	}
+	return true
+}
+
+// ComputerExtensionAttributes returns all computer extension attributes
+func (j *Client) ComputerExtensionAttributes() (*CompterExtensionAttributes, error) {
+	ep := fmt.Sprintf("%s/%s", j.Endpoint, computerExtAttrContext)
+	req, err := http.NewRequestWithContext(context.Background(), "GET", ep, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "error building JAMF computer extension attribute query request")
+	}
+
+	res := &CompterExtensionAttributes{}
+	if err := j.makeAPIrequest(req, &res); err != nil {
+		return nil, errors.Wrapf(err, "unable to query computer extension attribute from %s", ep)
+	}
+	return res, nil
+}
+
+// ComputerExtensionAttributeDetails returns the details for a specific computer extension attribute given its ID or Name
+func (j *Client) ComputerExtensionAttributeDetails(identifier interface{}) (*ComputerExtensionAttributeDetails, error) {
+	ep, err := EndpointBuilder(j.Endpoint, computerExtAttrContext, identifier)
+	if err != nil {
+		return nil, errors.Wrapf(err, "error building JAMF query request endpoint for script: %v", identifier)
+	}
+	req, err := http.NewRequestWithContext(context.Background(), "GET", ep, nil)
+	if err != nil {
+		return nil, errors.Wrapf(err, "error building JAMF query request for computer extension attribute: %v", identifier)
+	}
+
+	res := ComputerExtensionAttributeDetails{}
+	if err := j.makeAPIrequest(req, &res); err != nil {
+		return nil, errors.Wrapf(err, "unable to query computer extension attribute with ID: %v from %s", identifier, ep)
+	}
+
+	return &res, nil
+}
+
+// UpdateComputerExtensionAttribue will update a computer extension attribute in Jamf by either ID or Name
+func (j *Client) UpdateComputerExtensionAttribue(identifier interface{}, content *ComputerExtenstionAttribute) (*ComputerExtenstionAttribute, error) {
+	ep, err := EndpointBuilder(j.Endpoint, computerExtAttrContext, identifier)
+	if err != nil {
+		return nil, errors.Wrapf(err, "error building JAMF query request for computer extension attribute: %v", identifier)
+	}
+
+	err = ValidateComputerExtensionAttribute(content)
+	if err != nil {
+		return nil, errors.Wrapf(err, "computer extension attribute validation failed: %v", identifier)
+	}
+
+	bodyContent, err := xml.Marshal(content)
+	if err != nil {
+		return nil, errors.Wrapf(err, "error building JAMF update payload for computer extension attribute: %v", identifier)
+	}
+
+	body := bytes.NewReader(bodyContent)
+	req, err := http.NewRequestWithContext(context.Background(), "PUT", ep, body)
+	if err != nil {
+		return nil, errors.Wrapf(err, "error building JAMF update request for computer extension attribute: %v (%s)", identifier, ep)
+	}
+
+	res := ComputerExtenstionAttribute{}
+	if err := j.makeAPIrequest(req, &res); err != nil {
+		return nil, errors.Wrapf(err, "unable to process JAMF update request for computer extension attribute: %v (%s)", identifier, ep)
+	}
+
+	return &res, nil
+}
+
+// CreateComputerExtenstionAttribute will create a computer extension attribute in Jamf
+func (j *Client) CreateComputerExtenstionAttribute(content *ComputerExtenstionAttribute) (*ComputerExtenstionAttribute, error) {
+	// -1 denotes the next available ID
+	ep, err := EndpointBuilder(j.Endpoint, computerExtAttrContext, -1)
+	if err != nil {
+		return nil, errors.Wrapf(err, "error building JAMF query request for new computer extension attribute")
+	}
+
+	if content == nil {
+		return nil, errors.Wrapf(fmt.Errorf("Empty payload"), "unable to process JAMF creation request for computer extension attribute: (%s)", ep)
+	}
+
+	if content.Name == "" {
+		return nil, errors.Wrapf(fmt.Errorf("Name required for new computer extension attribute"), "unable to process JAMF creation request for computer extension attribute: (%s)", ep)
+	}
+
+	err = ValidateComputerExtensionAttribute(content)
+	if err != nil {
+		return nil, errors.Wrapf(err, "computer extension attribute validation failed: %v", content.Name)
+	}
+
+	bodyContent, err := xml.Marshal(content)
+	if err != nil {
+		return nil, errors.Wrapf(err, "error building JAMF creation payload for computer extension attribute: %v", content.Name)
+	}
+
+	body := bytes.NewReader(bodyContent)
+	req, err := http.NewRequestWithContext(context.Background(), "POST", ep, body)
+	if err != nil {
+		return nil, errors.Wrapf(err, "error building JAMF creation request for computer extension attribute: %v (%s)", content.Name, ep)
+	}
+
+	res := ComputerExtenstionAttribute{}
+	if err := j.makeAPIrequest(req, &res); err != nil {
+		return nil, errors.Wrapf(err, "unable to process JAMF creation request for computer extension attribute: %v (%s)", content.Name, ep)
+	}
+
+	return &res, nil
+}
+
+// DeleteComputerExtenstionAttribute will delete a computer extension attribute by either ID or Name
+func (j *Client) DeleteComputerExtenstionAttribute(identifier interface{}) (*ComputerExtenstionAttribute, error) {
+	ep, err := EndpointBuilder(j.Endpoint, computerExtAttrContext, identifier)
+	if err != nil {
+		return nil, errors.Wrapf(err, "error building JAMF query request for computer extension attribute: %v", identifier)
+	}
+
+	req, err := http.NewRequestWithContext(context.Background(), "DELETE", ep, nil)
+	if err != nil {
+		return nil, errors.Wrapf(err, "error building JAMF deletion request for computer extension attribute: %v (%s)", identifier, ep)
+	}
+
+	res := ComputerExtenstionAttribute{}
+	if err := j.makeAPIrequest(req, &res); err != nil {
+		return nil, errors.Wrapf(err, "unable to process JAMF deletion request for computer extension attribute: %v (%s)", identifier, ep)
+	}
+
+	return &res, nil
+}
---
+++ classic/computer_extenstion_attr_entity_test.go
@@ -0,0 +1,85 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2020 Datadog, Inc.
+
+package classic_test
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	jamf "github.com/DataDog/jamf-api-client-go/classic"
+	"github.com/stretchr/testify/assert"
+)
+
+var COMPUTER_EXT_ATTR_API_BASE_ENDPOINT = "/JSSResource/computerextensionattributes"
+
+func computerExtResponseMocks(t *testing.T) *httptest.Server {
+	var resp string
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch r.RequestURI {
+		case COMPUTER_EXT_ATTR_API_BASE_ENDPOINT:
+			fmt.Fprintf(w, `{
+				"scripts": [
+					{
+							"id": 33,
+							"name": "Check Firewall"
+					},
+					{
+							"id": 99,
+							"name": "Is Logged In User Admin"
+					},
+					{
+							"id": 103,
+							"name": "Team"
+					}]
+			}`)
+		case fmt.Sprintf("%s/id/33", COMPUTER_EXT_ATTR_API_BASE_ENDPOINT), fmt.Sprintf("%s/id/-1", COMPUTER_EXT_ATTR_API_BASE_ENDPOINT), fmt.Sprintf("%s/name/Check Firewall", COMPUTER_EXT_ATTR_API_BASE_ENDPOINT):
+			switch r.Method {
+			case "PUT", "POST":
+				data, err := ioutil.ReadAll(r.Body)
+				if err != nil {
+					fmt.Fprintf(w, err.Error())
+				}
+				compExtAttrContents := &jamf.ComputerExtenstionAttribute{}
+				err = xml.Unmarshal(data, compExtAttrContents)
+				if err != nil {
+					fmt.Fprintf(w, err.Error())
+				}
+				compExtData, err := json.MarshalIndent(compExtAttrContents, "", "    ")
+				if err != nil {
+					fmt.Fprintf(w, err.Error())
+				}
+				fmt.Fprintf(w, string(compExtData))
+			default:
+				mockCompExtAttr := &jamf.ComputerExtenstionAttribute{
+					ID:          33,
+					Name:        "Check Firewall",
+					Enabled:     true,
+					Description: "Checks to ensure firewall is enabled on client",
+					DataType:    "String",
+					InputType: &jamf.ComputerExtensionAttrInputType{
+						Type: "",
+					},
+					InventoryDisplay: "Operating System",
+					ReconDisplay:     "Extension Attributes",
+				}
+
+				compExtData, err := json.MarshalIndent(mockCompExtAttr, "", "    ")
+				if err != nil {
+					fmt.Fprintf(w, err.Error())
+				}
+				fmt.Fprintf(w, string(compExtData))
+			}
+		default:
+			http.Error(w, fmt.Sprintf("bad Jamf API %s call to %s", r.Method, r.URL), http.StatusInternalServerError)
+			return
+		}
+		_, err := w.Write([]byte(resp))
+		assert.Nil(t, err)
+	}))
+}
