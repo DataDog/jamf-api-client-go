ce82bac4660306abcbf4655d31372d8256c4f59c
Szymon Wojtyniak <szymon.wojtyniak@ravelin.com>
2024-07-31 14:53:47 +0100

    create/delete groups, unit tests for each

--- classic/computer_group_test.go
+++ classic/computer_group_test.go
@@ -3,6 +3,7 @@ package classic_test
 import (
 	"encoding/xml"
 	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -36,23 +37,61 @@ func computerGroupsResponseMocks(t *testing.T) *httptest.Server {
 							"is_smart": false
 					}]
 				}`)
-		case fmt.Sprintf("%s/id/1", COMPUTER_GROUPS_BASE_API_ENDPOINT), fmt.Sprintf("%s/name/Test%sGroup%s1", COMPUTER_GROUPS_BASE_API_ENDPOINT, "%20", "%20"):
+		case fmt.Sprintf("%s/id/1", COMPUTER_GROUPS_BASE_API_ENDPOINT), fmt.Sprintf("%s/id/-1", COMPUTER_GROUPS_BASE_API_ENDPOINT), fmt.Sprintf("%s/name/Test%sGroup%s1", COMPUTER_GROUPS_BASE_API_ENDPOINT, "%20", "%20"):
 			switch r.Method {
 			case "GET":
 				w.Header().Add("Content-Type", "application/xml")
 				mockGroup := &jamf.ComputerGroup{
-					jamf.ComputerGroupDetails{
-						jamf.BasicComputerGroupInfo{
+					Info: jamf.ComputerGroupDetails{
+						BasicComputerGroupInfo: jamf.BasicComputerGroupInfo{
 							ID:      1,
 							Name:    "Test Group 1",
 							IsSmart: false,
 						},
-						[]jamf.BasicComputerInfo{
+						Computers: []jamf.BasicComputerInfo{
 							{
 								GeneralInformation: jamf.GeneralInformation{
 									ID:   1,
 									Name: "Test Computer 1",
 								},
+							},
+						},
+					},
+				}
+				groupData, err := xml.MarshalIndent(mockGroup, "", "  ")
+				if err != nil {
+					fmt.Fprintf(w, err.Error())
+				}
+				fmt.Fprintf(w, string(groupData))
+			case "POST":
+				w.Header().Add("Content-Type", "application/xml")
+				data, err := io.ReadAll(r.Body)
+				if err != nil {
+					fmt.Fprintf(w, err.Error())
+				}
+				groupContents := &jamf.ComputerGroupDetails{}
+				err = xml.Unmarshal(data, groupContents)
+				if err != nil {
+					fmt.Fprintf(w, err.Error())
+				}
+				groupData, err := xml.MarshalIndent(groupContents, "", "  ")
+				if err != nil {
+					fmt.Fprintf(w, err.Error())
+				}
+				fmt.Fprintf(w, string(groupData))
+			default:
+				w.Header().Add("Content-Type", "application/xml")
+				mockGroup := &jamf.ComputerGroupDetails{
+					BasicComputerGroupInfo: jamf.BasicComputerGroupInfo{
+						ID:      1,
+						Name:    "Test Group 1",
+						IsSmart: false,
+					},
+					Computers: []jamf.BasicComputerInfo{
+						{
+							GeneralInformation: jamf.GeneralInformation{
+								ID:   1,
+								Name: "Test Computer 1",
 							},
 						},
 					},
@@ -103,4 +142,41 @@ func TestQuerySpecificComputerGroups(t *testing.T) {
 	assert.Equal(t, false, grp.Info.IsSmart)
 	assert.Equal(t, 1, grp.Info.Computers[0].ID)
 	assert.Equal(t, "Test Computer 1", grp.Info.Computers[0].Name)
+}
+
+func TestCreateComputerGroup(t *testing.T) {
+	server := computerGroupsResponseMocks(t)
+	defer server.Close()
+	j, err := jamf.NewClient(server.URL, "test", "test", server.Client(), jamf.WithTokenAuth())
+	assert.Nil(t, err)
+	grp := &jamf.ComputerGroupDetails{
+		BasicComputerGroupInfo: jamf.BasicComputerGroupInfo{
+			Name:    "Unit Test Group",
+			IsSmart: false,
+		},
+		Computers: []jamf.BasicComputerInfo{},
+	}
+	createdGrp, err := j.CreateComputerGroup(grp)
+	assert.Nil(t, err)
+	assert.Equal(t, "Unit Test Group", createdGrp.Name)
+	assert.Equal(t, false, createdGrp.IsSmart)
+	assert.Equal(t, 0, len(createdGrp.Computers))
+}
+
+func TestDeleteComputerGroup(t *testing.T) {
+	server := computerGroupsResponseMocks(t)
+	defer server.Close()
+	j, err := jamf.NewClient(server.URL, "test", "test", server.Client(), jamf.WithTokenAuth())
+	assert.Nil(t, err)
+	deletedGrp, err := j.DeleteComputerGroup(1)
+	assert.Nil(t, err)
+	assert.Equal(t, 1, deletedGrp.ID)
+	assert.Equal(t, "Test Group 1", deletedGrp.Name)
+	assert.Equal(t, false, deletedGrp.IsSmart)
+
+	deletedGrp, err = j.DeleteComputerGroup("Test%20Group%201")
+	assert.Nil(t, err)
+	assert.Equal(t, 1, deletedGrp.ID)
+	assert.Equal(t, "Test Group 1", deletedGrp.Name)
+	assert.Equal(t, false, deletedGrp.IsSmart)
 }
